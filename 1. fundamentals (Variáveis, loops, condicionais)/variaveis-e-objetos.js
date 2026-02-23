d8d57359572e101b32805d5d94c47bdf7d11cfd0
ElisangelaJesus <elisa_js@hotmail.com>
2026-02-23 19:04:37 -0300

    feat: pratica de imutabilidade e tipos de dados em JS

---
+++ 1. fundamentals (Variáveis, loops, condicionais)/variaveis-e-objetos.js
@@ -0,0 +1,19 @@
+// 1. Declare uma constante para o seu nome
+const nome = "Elisangela";
+
+// 2. Declare uma variável para sua idade (que pode mudar)
+let idade = 31; // Coloque sua idade real aqui
+
+// 3. Crie o objeto de usuário
+const usuario = {
+  nome: nome,
+  idade: idade,
+  posgraduando: true,
+  tecnologias: ["JavaScript", "Git", " GitHub"],
+};
+
+// 4. Tente mudar o nome (Isso vai causar um erro proposital)
+// nome = "Outro Nome";
+//Explicação: "Isso deu erro porque constantes não permitem reatribuição".
+
+console.log("Perfil do Usuário:", usuario);
